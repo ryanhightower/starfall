99586b42a25637096ffe8e81bae680b645b284a3
Ryan Hightower <ryan@ryanhightower.com>
2014-02-26 09:49:25 -0700

    Changed price-quote.php to checkout-po.php
    to better distinguish the checkout cart function, renamed price-quote
    to checkout and made two files for cc checkout and po checkout.

---
+++ checkout-cc.php
@@ -0,0 +1,243 @@
+
+<?php include("includes/functions.php");
+
+get_header_inner(); 
+
+// Intermediate EDUCATOR Store Page
+// Description: This page asks the user what payment method they wish to use.
+// Options are "Institution (P.O.)", "Personal (CC)", "Offline (Mail, fax, phone). 
+// After the user makes their selection, the page should redirect them to the original
+// route they were going to. 
+?>
+
+	<div class="container">
+    
+		<header>
+        	<div class="row">
+            <div class="col-sm-2">
+			<!-- Starfall logo -->
+			<div id="logo"><h3>Starfall</h3></div>
+            </div>
+            <div class="col-sm-7">
+				<h1>Checkout Credit Card</h1>
+            </div>
+            
+            <div class="newClear"></div>
+
+            </div>
+		</header>
+
+        <section class="container">
+        <div class="row">
+        <div class="col-sm-12">		
+           <div class="row">
+           		<div>
+                	<div class="col-sm-4 fr">Establish an account based on this PO and speed up delivery of yourproduct!
+               		</div>
+                    <div class="newClear"></div>
+                </div>
+                <div class="padsim4">
+                	<div class="col-sm-8">
+                    	<h2>Price Quote #123456</h2>
+                        <span>Your quote has been generated successfully. View your details below and print or email to give to your Administrator.</span>
+               		</div>
+                	<div class="col-sm-4 fr">
+                    	<a class="btn btn-primary btn-lg" href="">Pay Now by CC</a>&nbsp;&nbsp;<a class="btn btn-primary btn-lg" href="">Create Account</a>
+               		</div>
+                    <div class="newClear"></div>
+                </div>    
+                <div class="padsim4">
+                	<div class="col-sm-8">
+                    	<a href="#" class="print">Print</a>
+                        <a href="#" class="email">Email</a>
+               		</div>
+                	
+                    <div class="newClear"></div>
+                </div>            
+                <div class="grey-box3">
+                             <div class="padsim1">
+                                <div class="col-sm-10">
+                                <div class="simplleBoldstyle1">Quote #1234567</div></div>
+                                <div class="col-sm-2">Price</div>
+                                <div class="newClear"></div>
+                            </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div> 
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div class="padsim2">
+                                <div class="col-sm-10">
+                                    <div class="studItemBox">
+                                        <span><img data-src="holder.js/25x25" alt="25x25" class="img-circle img-center img-responsive"></span>
+                                        <div><strong>Product Item 1</strong><br />
+                                        Short description of the product and what it does,why it’s useful or fun, etc.
+                                        </div> 
+                                        <div class="newClear"></div>
+                                    </div>
+                                </div>                       
+                                <div class="col-sm-2">
+                                <div class="rightfinshedBox">
+                                <span class="moresapce">$4.99</span><span>21</span>
+                                <div class="newClear"></div>
+                                </div>
+                                </div>                        
+                                <div class="newClear"></div>
+                             </div>
+                             <div>
+                             <div class="col-sm-6">
+								NOTICE:Quotes are good for <strong>30days</strong> after which the quote number will expire.If you need to adjustorre printyour quote,you may view create a quote from your<strong> purchase history</strong>. Call us at 1-800-999-9999 with questions orcomments.
+        					 </div>
+                             <div class="col-sm-3 fr">
+                                <div class="totalBox2">
+                                    <div class="totlaBar">
+                                        <strong>SubTotal:</strong><span>$544.99</span>
+                                    </div>
+                                    <div class="totlaBar">
+                                        <strong>Shipping:</strong><span>$16.99</span>
+                                    </div>
+                                    <div class="totlaBar">
+                                        <strong>Tax</strong><span>$0.00</span>
+                                    </div>
+                                    <div class="totlaBar2">
+                                        <strong>Total</strong><span>$561.98</span>
+                                    </div>
+                                </div>
+        					  </div>
+        					 </div>
+                             <div class="newClear"></div>
+               </div>            
+           </div>
+         </div>        
+        </div> 
+                    
+        </section>
+
+		
+		<div class="clearfix"></div>
+
+
+
+
+<?php //get_footer(); ?>
